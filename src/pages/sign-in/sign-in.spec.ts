5ba75f9ee1100b5e231d6730ee185f78fd55ae16
Rob Ferguson <rob.ferguson@robferguson.org>
2017-11-26 17:52:23 +1100

    Updated the README.md file

--- src/pages/sign-in/sign-in.spec.ts
+++ src/pages/sign-in/sign-in.spec.ts
@@ -2,8 +2,8 @@ import { ComponentFixture, async } from '@angular/core/testing';
 
 import { TestUtils }               from '../../test';
 
-// import { SignInPage } from './sign-in';
-import { SignInPage } from '@pages/sign-in/sign-in';
+import { SignInPage } from './sign-in';
+// import { SignInPage } from '@pages/sign-in/sign-in';
 
 let fixture: ComponentFixture<SignInPage> = null;
 let instance: any = null;
