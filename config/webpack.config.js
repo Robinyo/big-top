bc6f7a63ee369844671ae5758ee2927ff9def691
Rob Ferguson <rob.ferguson@robferguson.org>
2017-11-28 18:23:06 +1100

    Fixed all lint warnings

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -33,7 +33,7 @@ if (env === 'prod' || env === 'dev') {
 
 function environmentPath() {
 
-  let filePath = './src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
+  let filePath = '../src/environments/environment' + (env === 'prod' ? '' : '.' + env) + '.ts';
 
   if (!fs.existsSync(filePath)) {
     console.log(chalk.red('\n' + filePath + ' does not exist!'));
