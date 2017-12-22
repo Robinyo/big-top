0b66a6fac532e8a81ddd355a05e5b84489d78366
Rob Ferguson <rob.ferguson@robferguson.org>
2017-12-22 16:44:39 +1100

    Added Santa state animation

--- src/pages/animations/sliding-entrances/santa-state.animation.ts
+++ src/pages/animations/sliding-entrances/santa-state.animation.ts
@@ -17,6 +17,34 @@ export const SANTA_STATE_ANIMATION =
       style({
         transform: 'translateX(-100%)'
       }),
+      animate('200ms ease-in')
+    ]),
+    transition('in => out', [
+      animate('200ms ease-out', style({
+        transform: 'translateX(100%)'
+      }))
+    ])
+  ]);
+
+/*
+
+export const SANTA_STATE_ANIMATION =
+  trigger('santaState', [
+    state('in', style({
+      opacity: 1,
+      transform: 'translateX(0)'})),
+    state('out', style({
+      opacity: 0})),
+    transition('void => *', [
+      style({
+        transform: 'translateX(-100%)'
+      }),
+      animate('2s ease-in')
+    ]),
+    transition('out => in', [
+      style({
+        transform: 'translateX(-100%)'
+      }),
       animate('100ms ease-in')
     ]),
     transition('in => out', [
@@ -26,40 +54,30 @@ export const SANTA_STATE_ANIMATION =
     ])
   ]);
 
-/*
-
-animations: [
-  trigger('heroState', [
-    state('inactive', style({
-      backgroundColor: '#eee',
-      transform: 'scale(1)'
-    })),
-    state('active',   style({
-      backgroundColor: '#cfd8dc',
-      transform: 'scale(1.1)'
-    })),
-    transition('inactive => active', animate('100ms ease-in')),
-    transition('active => inactive', animate('100ms ease-out'))
-  ])
-]
-
-export const santaState =
+export const SANTA_STATE_ANIMATION =
   trigger('santaState', [
-      state('in', style({opacity: 1, transform: 'translateX(0)'})),
-      transition('void => *', [
-        style({
-          opacity: 0,
-          transform: 'translateX(-100%)'
-        }),
-        animate('1s 100ms')
-      ]),
-      transition('* => void', [
-        animate('1s 100ms', style({
-          opacity: 0,
-          transform: 'translateX(100%)'
-        }))
-      ])
-    ]
-  );
+    state('in', style({
+      opacity: 1,
+      transform: 'translateX(0)'})),
+    state('out', style({
+      opacity: 0})),
+    transition('void => *', [
+      style({
+        transform: 'translateX(100%)'
+      }),
+      animate('2s ease-in')
+    ]),
+    transition('out => in', [
+      style({
+        transform: 'translateX(100%)'
+      }),
+      animate('2s ease-in')
+    ]),
+    transition('in => out', [
+      animate('2s ease-out', style({
+        transform: 'translateX(-100%)'
+      }))
+    ])
+  ]);
 
 */
